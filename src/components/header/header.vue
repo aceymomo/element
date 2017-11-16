049fd00fcbbae79c7ca87678b4b7a7fa18187845
aceymomo <kobezhang333@vip.qq.com>
2017-11-16 20:28:41 +0800

    1116 02

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -29,7 +29,15 @@
      <div class="background">
        <img :src="seller.avatar" width="100%" height="100%">
      </div>
-     <div v-show="detailShow" class="detail"></div>
+     <div v-show="detailShow" class="detail">
+       <div class="detail-wrapper clearfix">
+         <div class="detail-main">
+         </div>
+       </div>
+       <div class="detail-close">
+         <i class="icon-close"></i>
+       </div>
+     </div>
  </div>
 </template>
 
@@ -178,6 +186,19 @@
     left:0
     width:100%
     height:100%
+    overflow :auto
     z-index :100
     background:rgba(7,17,27,0.8)
+    .detail-wrapper
+      min-height :100%
+      .detail-main
+        margin-top :64px
+        padding-bottom :64px
+    .detail-close
+      position :relative
+      width:32px
+      height :32px
+      margin:-64px auto 0 auto
+      clear:both
+      font-size :32px
 </style>
