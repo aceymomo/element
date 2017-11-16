58cc2884bab8c92418ce9f91e151c818053abc8a
aceymomo <kobezhang333@vip.qq.com>
2017-11-16 17:00:13 +0800

    1116

--- src/App.vue
+++ src/App.vue
@@ -54,7 +54,6 @@ export default {
   -webkit-font-smoothing: antialiased
   -moz-osx-font-smoothing: grayscale
   color: #2c3e50
-  margin-top: 60px
   .tab
     display:flex
     width:100%
--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -13,12 +13,23 @@
            {{seller.description}}/{{seller.deliveryTime}}分钟送达
          </div>
          <div v-if="seller.supports" class="support">
-           <span class="icon"></span>
-           <span class="text">{{seller.supports[0].description}}</span>
+           <span class="icon" :class="classMap[seller.supports[1].type]"></span>
+           <span class="text">{{seller.supports[1].description}}</span>
          </div>
        </div>
+       <div v-if="seller.supports" class="support-count" @click="showDetail">
+         <span class="count">{{seller.supports.length}}</span>
+         <i class="icon-keyboard_arrow_right"></i>
+       </div>
      </div>
-     <div class="bulletin-wrapper"></div>
+     <div class="bulletin-wrapper" @click="showDetail">
+       <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+       <i class="icon-keyboard_arrow_right"></i>
+     </div>
+     <div class="background">
+       <img :src="seller.avatar" width="100%" height="100%">
+     </div>
+     <div v-show="detailShow" class="detail"></div>
  </div>
 </template>
 
@@ -29,11 +40,19 @@
        type:Object
      }
    },
+   created () {
+     this.classMap = ['decrease','discount','guarantee','invoice','special']
+   },
    data() {
      return {
-
+       detailShow:false
      }
 
+   },
+   methods:{
+     showDetail(){
+       this.detailShow = true
+     }
    },
    components: {
 
@@ -45,16 +64,21 @@
 @import '../../common/stylus/mixin'
 .header
   color:#fff
-  background :#000
+  position :relative
+  background :rgba(7,17,27,0.5)
+  overflow: hidden
   .content-wrapper
+    position: relative
     padding:24px 12px 18px 24px
     font-size:0
     .avatar
       display :inline-block
+      vertical-align :top
+      img
+        border-radius :2px
     .content
       display :inline-block
       margin-left :16px
-      font-size :14px
       .title
         margin:2px 0 8px 0
         .brand
@@ -70,4 +94,90 @@
           font-size:16px
           line-height :18px
           font-weight :bold
+      .description
+        margin-bottom:10px
+        line-height :12px
+        font-size :12px
+      .support
+        .icon
+          display :inline-block
+          vertical-align :top
+          width:12px
+          height :12px
+          margin-right :4px
+          background-size :12px 12px
+          background-repeat :no-repeat
+          &.decrease
+            bg-image('decrease_1')
+          &.discount
+            bg-image('discount_1')
+          &.guarantee
+            bg-image('guarantee_1')
+          &.invoice
+            bg-image('invoice_1')
+          &.special
+            bg-image('special_1')
+        .text
+          line-height :12px
+          font-size :10px
+    .support-count
+      position :absolute
+      right:12px
+      bottom:14px
+      padding:0 8px
+      height:24px
+      line-height :24px
+      border-radius :14px
+      background :rgba(0,0,0,0.2)
+      text-align :center
+      .count
+        vertical-align :top
+        font-size :10px
+      .icon-keyboard_arrow_right
+        margin-left :2px
+        line-height :24px
+        font-size :10px
+  .bulletin-wrapper
+    position :relative
+    height :28px
+    line-height :28px
+    padding:0 22px 0 12px
+    white-space :nowrap
+    overflow :hidden
+    text-overflow :ellipsis
+    background:rgba(7,17,27,0.2)
+    .bulletin-title
+      display :inline-block
+      vertical-align :top
+      margin-top:8px
+      width :22px
+      height :12px
+      bg-image('bulletin')
+      background-size:22px 12px
+      background-repeat :no-repeat
+    .bulletin-text
+      vertical-align :top
+      margin:0 4px
+      font-size:10px
+    .icon-keyboard_arrow_right
+      position :absolute
+      right:12px
+      top:8px
+      font-size :10px
+  .background
+    position :absolute
+    top:0
+    left:0
+    width:100%
+    height:100%
+    z-index :-1
+    filter :blur(10px)
+  .detail
+    position :fixed
+    top:0
+    left:0
+    width:100%
+    height:100%
+    z-index :100
+    background:rgba(7,17,27,0.8)
 </style>
