89c0edf0b8a269c6e2d9ad81387617a9e299813e
aceymomo <kobezhang333@vip.qq.com>
2017-11-18 16:57:02 +0800

    1118

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -29,19 +29,40 @@
      <div class="background">
        <img :src="seller.avatar" width="100%" height="100%">
      </div>
-     <div v-show="detailShow" class="detail">
-       <div class="detail-wrapper clearfix">
-         <div class="detail-main">
-           <h1 class="name">{{seller.name}}</h1>
-           <div class="star-wrapper">
-             <star :size="48" :score="seller.score"></star>
-           </div>
-         </div>
-       </div>
-       <div class="detail-close">
-         <i class="icon-close"></i>
-       </div>
-     </div>
+    <transition name="fade">
+      <div v-show="detailShow" class="detail">
+        <div class="detail-wrapper clearfix">
+          <div class="detail-main">
+            <h1 class="name">{{seller.name}}</h1>
+            <div class="star-wrapper">
+              <star :size="48" :score="seller.score"></star>
+            </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul v-if="seller.supports" class="supports">
+              <li class="support-item" v-for="(item,index) in seller.supports">
+                <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+                <span class="text">{{seller.supports[index].description}}</span>
+              </li>
+            </ul>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div class="bulletin">
+              <p class="content">{{seller.bulletin}}</p>
+            </div>
+          </div>
+        </div>
+        <div class="detail-close" @click="hideDetail">
+          <i class="icon-close"></i>
+        </div>
+      </div>
+    </transition>
  </div>
 </template>
 
@@ -58,13 +79,16 @@ import star from '@/components/star/star'
    },
    data() {
      return {
-       detailShow:false
+       detailShow:false,
      }
 
    },
    methods:{
      showDetail(){
        this.detailShow = true
+     },
+     hideDetail(){
+       this.detailShow = false
      }
    },
    components: {
@@ -185,6 +209,14 @@ import star from '@/components/star/star'
     height:100%
     z-index :-1
     filter :blur(10px)
+  .fade-enter
+    opacity :0
+    background:rgba(7,17,27,0)
+  .fade-enter-active,.fade-leave-active
+    transition :all 0.5s
+  .fade-leave-to
+    background:rgba(7,17,27,0.8)
+    opacity:0
   .detail
     position :fixed
     top:0
@@ -194,6 +226,7 @@ import star from '@/components/star/star'
     overflow :auto
     z-index :100
     background:rgba(7,17,27,0.8)
+    backdrop-filter:blur(10px)
     .detail-wrapper
       min-height :100%
       width:100%
@@ -209,6 +242,56 @@ import star from '@/components/star/star'
           margin-top:18px
           padding:2px 0
           text-align :center
+        .title
+          display :flex
+          width:80%
+          margin:28px auto 24px auto
+          .line
+            flex:1
+            position :relative
+            top:-6px
+            border-bottom :1px solid rgba(255,255,255,0.2)
+          .text
+            padding:0 12px
+            font-weight :700
+            font-size :14px
+        .supports
+          width:80%
+          margin:0 auto
+          .support-item
+            padding:0 12px
+            margin-bottom :12px
+            font-size:0
+            &:last-child
+              margin-bottom :0
+            .icon
+              display :inline-block
+              width:16px
+              height:16px
+              vertical-align :top
+              margin-right :6px
+              background-size :16px 16px
+              background-repeat :no-repeat
+              &.decrease
+                bg-image('decrease_2')
+              &.discount
+                bg-image('discount_2')
+              &.guarantee
+                bg-image('guarantee_2')
+              &.invoice
+                bg-image('invoice_2')
+              &.special
+                bg-image('special_2')
+            .text
+              line-height :16px
+              font-size :12px
+        .bulletin
+          width:80%
+          margin:0 auto
+          .content
+            padding:0 12px
+            line-height :24px
+            font-size:12px
     .detail-close
       position :relative
       width:32px
