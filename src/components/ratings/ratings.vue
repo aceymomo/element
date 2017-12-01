f809388075f510563b5137bf37975af9e9ef4874
aceymomo <kobezhang333@vip.qq.com>
2017-12-01 20:04:05 +0800

    1201

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,5 +1,5 @@
 <template>
- <div class="ratings">
+ <div class="ratings" ref="ratings">
      <div class="ratings-content">
        <div class="overview">
          <div class="overview-left">
@@ -29,23 +29,23 @@
         :ratings="ratings" @ratingtype="ratingtypes" @toggletype="toggletypes"></ratingselect>
        <div class="rating-wrapper">
          <ul>
-           <li v-for="rating in ratings" class="rating-item">
+           <li v-for="rating in ratings" class="rating-item" v-show="needShow(rating.rateType,rating.text)">
             <div class="avatar">
               <img width="28" height="28" :src="rating.avatar">
             </div>
             <div class="content">
               <div class="usertime">
                 <h1 class="title">{{rating.username}}</h1>
-                <span class="time">{{rating.rateTime}}</span>
+                <span class="time">{{rating.rateTime | formatData}}</span>
               </div>
               <div class="startime">
                 <star :size="24" :score="rating.score"></star>
-                <span class="times">{{rating.deliveryTime}}分钟送达</span>
+                <span class="times" v-show="rating.deliveryTime">{{rating.deliveryTime}}分钟送达</span>
               </div>
               <p class="text">{{rating.text}}</p>
               <div class="iconbox">
                 <i class="icon-thumb_up" v-show="rating.recommend"></i>
-                <ul>
+                <ul class="recomcon">
                   <li v-show="rating.recommend" v-for="recom in rating.recommend" class="recomBox">
                     <span class="recomment">{{recom}}</span>
                   </li>
@@ -60,9 +60,11 @@
 </template>
 
 <script type="text/ecmascript-6">
+import BScroll from 'better-scroll'
 import star from '@/components/star/star'
 import split from '@/components/split/split'
 import ratingselect from '@/components/ratingselect/ratingselect'
+import {formatData} from '@/common/js/date'
 const ALL = 2
 const ERR_OK = 0
  export default {
@@ -89,19 +91,46 @@ const ERR_OK = 0
      .then(res=>{
        if(res.data.errno === ERR_OK){
          this.ratings = res.data.data
+         this.$nextTick(()=>{
+           this.scroll = new BScroll(this.$refs.ratings,{
+             click:true
+           })
+         })
        }
      })
      .catch(error=>{
        console.log(error)
      })
    },
+   filters:{
+        formatData(time){
+            let data = new Date(time)
+            return formatData(data,'yyyy-MM-dd hh:mm')
+        }
+    },
    methods:{
       ratingtypes(val){
           this.selectType = val
+          this.$nextTick(()=>{
+              this.scroll.refresh()
+          })
       },
       toggletypes(){
           this.onlyContent = !this.onlyContent
+          this.$nextTick(()=>{
+              this.scroll.refresh()
+          })
       },
+      needShow(type,text){
+          if(this.onlyContent && !text){
+              return false
+          }
+          if(this.selectType === ALL){
+              return true
+          }else{
+              return type === this.selectType
+          }
+      }
    },
    components: {
      star,
@@ -224,13 +253,17 @@ const ERR_OK = 0
           .icon-thumb_up
             font-size:12px
             color:rgb(0,160,220)
-            line-height :16px
+            line-height :22px
             margin-right :8px
+          .recomcon
+            display:flex
+            flex-wrap:wrap
           .recomBox
-            float :left
             margin-right:8px
+            margin-bottom :10px
+            font-size:0
             .recomment
-              padding:6px
+              padding:4px 6px
               font-size:9px
               color:rgb(147,153,159)
               line-height :16px
--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,24 +1,211 @@
 <template>
- <div>
-   wo shi seller
+ <div class="seller" ref="seller">
+   <div class="seller-content">
+     <div class="overview">
+       <h1 class="title">{{seller.name}}</h1>
+       <div class="desc">
+         <star :size="36" :score="seller.score"></star>
+         <span class="text">({{seller.ratingCount}})</span>
+         <span class="text">月售{{seller.sellCount}}单</span>
+       </div>
+     </div>
+     <div class="overul">
+       <ul class="blockbox">
+         <li class="block">
+           <h2>起送价</h2>
+           <div class="content">
+             <span class="stress">{{seller.minPrice}}</span>元
+           </div>
+         </li>
+         <li class="block">
+           <h2>商家配送</h2>
+           <div class="content">
+             <span class="stress">{{seller.deliveryPrice}}</span>元
+           </div>
+         </li>
+         <li class="block">
+           <h2>平均配送时间</h2>
+           <div class="content">
+             <span class="stress">{{seller.deliveryTime}}</span>分钟
+           </div>
+         </li>
+       </ul>
+     </div>
+     <split></split>
+     <div class="activity">
+       <h1 class="title">公告与活动</h1>
+       <p class="text">{{seller.bulletin}}</p>
+     </div>
+     <ul class="discountbox">
+       <li v-for="support in seller.supports" class="discount-wrapper">
+         <i class="icon" :class="classMap[support.type]"></i>
+         <span class="text">{{support.description}}</span>
+       </li>
+     </ul>
+     <split></split>
+     <div class="real">
+       <h1 class="title">商家实景</h1>
+       <ul class="realbox">
+         <li v-for="item in seller.pics" class="real-wrapper">
+           <img width="120" height="90" :src="item" class="realimg">
+         </li>
+       </ul>
+     </div>
+     <split></split>
+   </div>
  </div>
 </template>
 
 <script type="text/ecmascript-6">
+import BScroll from 'better-scroll'
+import star from '@/components/star/star'
+import split from '@/components/split/split'
  export default {
+   props:{
+     seller:{
+       type:Object
+     }
+   },
    data() {
      return {
        
      }
 
    },
+   created(){
+    this.classMap = ['decrease','discount','guarantee','invoice','special']
+    this.$nextTick(()=>{
+      if(!this.scroll){
+          this.scroll = new BScroll(this.$refs.seller,{
+              click:true
+          })
+      }else{
+          this.scroll.refresh()
+      }
+    })
+   },
    components: {
-
+     star,
+     split
    }
  }
 </script>
 
 <style scoped lang="stylus">
-
- 
+@import '../../common/stylus/mixin'
+.seller
+  position :fixed
+  top:174px
+  left:0
+  bottom:0
+  width:100%
+  overflow :hidden
+  .seller-content
+    margin:18px 0
+    .overview
+      margin:0 18px
+      padding-bottom:18px
+      border-1px(rgba(7,17,27,0.1))
+      font-size:0
+      .title
+        font-size:14px
+        color:rgb(7,17,27)
+        line-height :14px
+      .desc
+        display :flex
+        margin-top :8px
+        .star
+          line-height :18px
+          margin-right :8px
+        .text
+          font-size:10px
+          color:rgb(77,85,93)
+          line-height :18px
+          margin-right :12px
+    .overul
+      padding:18px 0
+      .blockbox
+        display :flex
+        justify-content :center
+        .block
+          flex:1
+          border-right:1px solid rgba(7,17,27,0.1)
+          text-align :center
+          &:last-child
+            border:none
+          h2
+            font-size:10px
+            color:rgb(147,153,159)
+            line-height :10px
+          .content
+            margin-top:4px
+            font-size:10px
+            font-weight :200
+            color:rgb(7,17,27)
+            line-height :24px
+            .stress
+              font-size:24px
+              font-weight :200
+              color:rgb(7,17,27)
+              line-height :24px
+    .activity
+      margin:18px 18px 0 18px
+      padding-bottom:16px
+      border-1px(rgba(7,17,27,0.1))
+      font-size:0
+      .title
+        margin-bottom :8px
+        font-size:14px
+        color:rgb(7,17,27)
+        line-height :14px
+      .text
+        margin:0 12px
+        font-size:12px
+        font-weight :200
+        color:rgb(240,20,20)
+        line-height :24px
+    .discountbox
+      padding:0 18px
+      .discount-wrapper
+        padding:16px 12px
+        border-1px(rgba(7,17,27,0.1))
+        &:last-child
+          border:none
+        .icon
+          display :inline-block
+          vertical-align :top
+          width:16px
+          height:16px
+          background-size :16px 16px
+          background-repeat :no-repeat
+          &.decrease
+            bg-image('decrease_4')
+          &.discount
+            bg-image('discount_4')
+          &.guarantee
+            bg-image('guarantee_4')
+          &.invoice
+            bg-image('invoice_4')
+          &.special
+            bg-image('special_4')
+        .text
+          font-size:12px
+          font-weight :200
+          color:rgb(7,17,27)
+          line-height :16px
+    .real
+      padding:18px
+      .title
+        margin-bottom :12px
+        font-size:14px
+        color:rgb(7,17,27)
+        line-height :14px
+      .realbox
+        display :flex
+        overflow :hidden
+        .real-wrapper
+          margin-right:6px
+          .realimg
+            width:120px
+            height:90px
 </style>
